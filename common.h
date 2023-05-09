01f4d32985a5022000c81b2de87fc9f96b39e21c
EthanLavi <esl225@lehigh.edu>
2023-05-09 16:50:38 -0400

    Committing common

---
+++ common.h
@@ -0,0 +1,15 @@
+#define CONTAINS 0
+#define INSERT 1
+#define REMOVE 2
+#define CNF_ELIST_SIZE 7 // 7
+#define CNF_PLIST_SIZE 128 // 128
+
+/// @brief IHT_Op is used by the Client Adaptor to pass in operations to Apply, by forming a stream of IHT_Ops.
+template <typename K, typename V>
+struct IHT_Op {
+    int op_type;
+    K key;
+    V value;
+    IHT_Op(int op_type_, K key_, V value_) : op_type(op_type_), key(key_), value(value_) {};
+};
+
